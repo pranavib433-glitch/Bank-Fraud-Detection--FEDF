e51cfeb785bc767d6f9f2703d4aecb2b61536bd6
pranavib433-glitch <pranavib433@gmail.com>
2026-06-19 10:52:07 +0530

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import FintechGuardian from "./FintechGuardian.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <FintechGuardian />
-  </React.StrictMode>
-);
